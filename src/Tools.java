b2ab72d8c0c1c36b98e430306dff35f12edadbef
jack125251 <111780288+jack125251@users.noreply.github.com>
2024-04-24 11:55:37 -0600

    Update Tools.java

--- src/Tools.java
+++ src/Tools.java
@@ -1,4 +1,26 @@
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
-public class Tools {
+import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
+abstract class Tools extends JPanel {
+	Point point;
+//	Point lastPoint;
+//	boolean m = false;
+
+	public Tools() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+//				lastPoint = new Point(e.getX(), e.getY());
+				point = new Point(e.getX(), e.getY());
+			}
+		});
+	}
 }
