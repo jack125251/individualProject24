3bf4f8709ad6c5ec8a61605fbf23626f87c190ca
jack125251 <111780288+jack125251@users.noreply.github.com>
2024-04-26 09:41:45 -0600

    Add files via upload

--- src/V2/SketchPanel.java
+++ src/V2/SketchPanel.java
@@ -1,4 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
 public class SketchPanel {
-
+	
+	SketchPanel(){
+		
+	}
+	
+	public void newCanvas() {
+		
+	}
+	
+	public void newInterface() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+//	public void save() {
+//		
+//	}
+//	
+//	public void load() {
+//		
+//	}
+	
+	public class AL extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			
+		}
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
 }
